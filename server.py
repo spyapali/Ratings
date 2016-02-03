8d43417661ad2b00dcbe12b83494e1941fc743d8
Hackbright Student <info@hackbrightacademy.com>
2016-02-02 18:03:39 -0800

    Adding login functionality

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -18,11 +18,45 @@ app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/login")
+def show_login_form():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route("/process_login", methods=["POST"])
+def process_user_login():
+    """Log in existing users, otherwise add new user."""
+
+    # EXISTING USERS
+    # adding userid to session. 
+    # Redirect back to homepage, with flash message saying "logged in"
+    # Logged out route is flash message, removing userid from session. 
+    username = request.form.get("username")
+    password = request.form.get("password")
+    user = User.query.filter_by(email=username).one()   # naming variables username vs. email?
+    user.user_id
+
+
+    # NEW USERS
+    # check if user with username exists in database (should not)
+    # add user to database
 
 
 if __name__ == "__main__":
